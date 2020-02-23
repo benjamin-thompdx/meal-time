f36adc859f978ba2987aa47a08b405ca21b35be2
Benjamin Thom <jaminthom@gmail.com>
2020-02-22 19:22:57 -0800

    refactor code. create custom method called AskWhatYouAteFor to DRY up code for user meal questions

--- MealTime.cs
+++ MealTime.cs
@@ -2,17 +2,21 @@ using System;
 
 class MealTime
 {
+  static void AskWhatYouAteFor(string meal)
+  {
+   Console.WriteLine("What did you eat for " + meal + "?"); 
+  }
   static void Main()
   {
-    Console.WriteLine("What did you eat for breakfast?");
+    AskWhatYouAteFor("breakfast");
     string yourBreakfast = Console.ReadLine();
     Console.WriteLine("You had " + yourBreakfast + " for breakfast.");
 
-    Console.WriteLine("What did you eat for lunch?");
+    AskWhatYouAteFor("lunch");
     string yourLunch = Console.ReadLine();
     Console.WriteLine("You had " + yourLunch + " for lunch.");
 
-    Console.WriteLine("What did you eat for dinner?");
+    AskWhatYouAteFor("dinner");
     string yourDinner = Console.ReadLine();
     Console.WriteLine("You had " + yourDinner + " for dinner.");
   }
